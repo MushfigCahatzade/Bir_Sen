ebbc8fa73fc8b54436f3ebac64d7ffd3386fce52
MushfigCahatzade <tu7mqdzpy@code.edu.az>
2022-03-25 02:13:08 +0400

    creat task 2

---
+++ FF-34/FF-34/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FF_34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius celsius = new Celsius(90);
+            Kelvin kelvin = celsius;
+            Console.WriteLine(kelvin.DegreeK);
+            
+        }
+    }
+    class Kelvin
+    {
+        public double DegreeK { get; set; }
+        public Kelvin(double degree)
+        {
+        DegreeK = degree;
+        }
+    }
+
+    class Celsius
+    {
+        public double DegreeC { get; set; }
+        public Celsius(double degree)
+        {
+            DegreeC = degree;
+        }
+        public static implicit operator Kelvin(Celsius d)
+        {
+            return new Kelvin(d.DegreeC + 273);
+        }
+    }
+}
