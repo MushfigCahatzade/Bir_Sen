6cfe0d288cc035fca744de5f5ffa5d0cace33f43
MushfigCahatzade <tu7mqdzpy@code.edu.az>
2022-04-04 23:07:51 +0400

    created task

---
+++ Task h/ConsoleApp35/App 35/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp35.App_35
+{
+    internal class Department
+    {
+        private static int _id;
+        public int Id { get; }
+        public string Name { get; set; }    
+        List<Employee> employees { get; set; }
+        public Department(string name)
+        {
+            _id++;
+            Id = _id;
+            Name = name;
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id: {Id} - Name: {Name} ");
+        }
+    }
+}
---
+++ Task h/ConsoleApp35/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp35.App_35;
+using System;
+
+namespace ConsoleApp35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Department omer = new Department("Yusifov");            
+            omer.ShowInfo();
+            Department musviq = new Department("Cahatzade");
+            musviq.ShowInfo();
+        }
+    }
+}
