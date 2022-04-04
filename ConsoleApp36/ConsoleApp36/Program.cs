65c2679536f94c1b265738782a5f6cb67ab79dff
MushfigCahatzade <tu7mqdzpy@code.edu.az>
2022-04-04 23:08:29 +0400

    created task

---
+++ ConsoleApp36/ConsoleApp36/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Loop1);
+            Thread thread2 = new Thread(Loop2);
+            thread1.Start();
+            thread2.Start();
+            Console.WriteLine("Hello Word");
+            thread1.Join();
+            thread2.Join();
+
+            Loop1();
+            Loop2();
+        }
+        static void Loop1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"Loop1 {i}");
+            }
+        }
+        static void Loop2()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine($"Loop2 {i}");
+            }
+        }
+
+    }
+}
